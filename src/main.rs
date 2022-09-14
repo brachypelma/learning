5d7b7434b0523a3a4d69f98b623ad2de011531dd
Eric Jonas <ericdjonas@gmail.com>
2022-09-14 06:16:49 -0400

    Convert degrees F to C

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  let mut degree = 32.0;
+  while degree <= 212.0 {
+    let celsius = fahrenheit_to_celsius(degree);
+    println!("{degree} degrees in fahrenheiht is {celsius} degrees in celsius");
+    degree += 1.0;
+  }
+}
+
+fn fahrenheit_to_celsius(deg: f64) -> f64 {
+  ((deg - 32.0) * 5.0) / 9.0
 }
